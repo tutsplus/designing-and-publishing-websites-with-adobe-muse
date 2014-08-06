88ed465c241cc31e041f8d33614a8c68e2adbe27
Ian Yates <ian@snaptin.com>
2014-08-06 17:54:34 +0200

    push exercise files

---
+++ exporthtml/assets/wind2/wind2_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-341960140");
